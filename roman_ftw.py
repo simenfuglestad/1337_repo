d2c84911fd2a26d23aa7afa29fff70a6c9de8d93
hggeren1337 <hggeren1337@gmail.com>
2015-02-24 12:39:45 +0100

    Updated roman_ftw.py with working subtraction function.

--- roman_ftw.py
+++ roman_ftw.py
@@ -25,7 +25,7 @@ def to_roman(n):
         while n >= integer:
             result += numeral
             n -= integer
-    print result
+#    print result
     return result
 
 
@@ -37,7 +37,7 @@ def from_roman(s):
     while s is not "":
         if "M" in s:
             result += 1000
-            s = s.re1place("M", "", 1)
+            s = s.replace("M", "", 1)
         elif "D" in s:
             result += 500
             s = s.replace ("D", "", 1)
@@ -57,53 +57,60 @@ def from_roman(s):
             result += 1
             s = s.replace ("I", "", 1)
 
-    print result
+#    print result
     return result
 
 def subtract_roman(a, b):
     a = remove_subtractives(a)
     b = remove_subtractives(b)
-    for c in b:
-        if c in a:
-            a = a.replace (c, "", 1)
-            b = b.replace (c, "", 1)
-    if b is not "":
-        while "M" in b or "D" in b or "C" in b or "L" in b or "X" in b or "V" in b:
-            a = a.replace ("M", "DD", 1)
-            a = a.replace ("D", "CCCCC", 1)
-            a = a.replace ("C", "LL", 1)
-            a = a.replace ("L", "XXXXX", 1)
-            a = a.replace ("X", "VV", 1)
-            a = a.replace ("V", "IIIII", 1)
-            for c in b:
-                if c in a:
-                    a = a.replace (c, "", 1)
-                    b = b.replace (c, "", 1)
+    while b is not "":
+        for c in b:
+            if c in a:
+                a = a.replace(c, "", 1)
+                b = b.replace(c, "", 1)
+        if b is not "":
+            if "M" in a:
+                a = a.replace("M", "DD", 1)
+            if "D" in a: 
+                a = a.replace("D", "CCCCC", 1)
+            if "C" in a: 
+                a = a.replace("C", "LL", 1)
+            if "L" in a: 
+                a = a.replace("L", "XXXXX", 1)
+            if "X" in a: 
+                a = a.replace("X", "VV", 1)
+            if "V" in a:
+                a = a.replace("V", "IIIII", 1)
     a = shorten(a)
-    print a
+#    print a
     return a
 
 
 def remove_subtractives(s):
     """Removes subtractives from the roman numeral."""
     while "CM" in s:
-        s = s.replace ("CM", "DCCCC", 1)
+        s = s.replace("CM", "DCCCC ", 1)
     while "CD" in s:
-        s = s.replace ("CD", "CCCC", 1)
+        s = s.replace("CD", "CCCC ", 1)
     while "XC" in s:
-        s = s.replace ("XC", "LXXXX", 1)
+        s = s.replace("XC", "LXXXX ", 1)
     while "XL" in s:
-        s = s.replace ("XL", "XXXX", 1)
+        s = s.replace("XL", "XXXX ", 1)
     while "IX" in s:
-        s = s.replace ("IX", "VIIII", 1)
+        s = s.replace("IX", "VIIII ", 1)
     while "IV" in s:
-        s = s.replace ("IV", "IIII", 1)
+        s = s.replace("IV", "IIII ", 1)
+#    print s
+    while " " in s:
+        s = s.replace(" ", "", 1)
+#    print s
     return s
     
 def shorten (p):
     """Shortens the roman number by replacing smaller numbers with larger ones."""
-    p = remove_subtractives(p)
-    p = ''.join(sorted(p, key=roman_numberal_map))
+    my_alphabet = "MDCLXVI"
+#    p = remove_subtractives(p)
+    p = "".join(sorted(p, key=lambda elem: [my_alphabet.index(c) for c in elem[0]]))
     while 5 * "I" in p:
         p = p.replace ("IIIII", "V", 1)
     while 2 * "V" in p:
@@ -116,10 +123,12 @@ def shorten (p):
         p = p.replace ("CCCCC", "D", 1)
     while 2 * "D" in p:
         p = p.replace ("DD", "M", 1)
+#    print p
     return p
 
-t1 = to_roman(3985)
-t2 = to_roman(199)
+#Testing whether all functions work:
+t1 = to_roman(200)
+t2 = to_roman(1115)
 t3 = subtract_roman(t1, t2)
 t4 =from_roman(t3)
-t4
+print t4
