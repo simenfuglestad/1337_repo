3a5d123b2488feac657e6bf2db3496cea9ca7bfe
sifusan <fuglestad.simen@gmail.com>
2015-03-01 15:00:09 +0100

    cleanup

--- lab3.py
+++
@@ -1,9 +0,0 @@
-from socket import*
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print "The server is ready to receive"
-while 1:
-    message, clientAdress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode('utf-8').upper().encode('utf-8')
-    serverSocket.sendto(modifiedMessage, clientAdress)
---
+++ lab4/lab4_prototype_test.py
@@ -0,0 +1,67 @@
+import random
+from sys import exit
+
+suits = ['spades', 'hearts', "diamonds", "clubs"]
+ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9',
+            '10', 'jack', 'queen', 'king']
+
+combination_table = [[0 for x in range(5)] for x in range(5)]
+#print combination_matrix
+
+
+deck_of_cards = [ (a, b) for a in ranks for b in suits ]
+
+class Deck(object):
+
+    def __init__(self, deck):
+        self.deck = deck
+        random.shuffle(deck)
+        print "The deck is shuffled \n"
+    
+    def start(self):
+        user_input = raw_input("How many players will be playing? > ")
+        print ""
+        
+        #   Add: Create new player objects here based on input
+        
+        if user_input.isdigit():
+            user_input = int(user_input)
+            if user_input > 1:
+                print "%s players will be playing\n" % user_input
+                print "Will now deal 5 cards to each player\n"
+            else:
+                print "You must enter a number of at least 2 players\n"
+                exit(0)
+ 
+        for i in range(user_input):
+            print "\nDealing hand number %s\n" % (i + 1)
+            new_hand = Hand.get_hand(Hand())
+    
+        
+        
+class Hand(object):
+    def __init__(self):
+        pass
+    
+    def get_hand(self):
+        
+        new_hand = []
+        
+        for c in range(5):
+            r = random.randint(0, len(newDeck.deck) -1)
+            new_hand.append(newDeck.deck[r])
+            
+        Player().show_hand(new_hand)
+                
+        
+class Player(object):
+    def __init__(self):
+        pass
+        
+    def show_hand(self, cards):
+        print "This is my hand"
+        print cards
+
+
+newDeck = Deck(deck_of_cards)
+newDeck.start()
