d4f1f75914537457ab9bd443eaee70d2a10f347d
sifusan <fuglestad.simen@gmail.com>
2015-05-14 20:29:56 +0200

    Added regular server/client

--- lab3/UDPServer.py
+++ lab3/UDPServer.py
@@ -7,6 +7,5 @@ serverSocket.bind(('', serverPort))
 print "The server is ready to receive"
 while 1:
     message, clientAdress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode('UTF-8').upper().encode('UTF-8')
-    
+    modifiedMessage = message.decode('UTF-8').upper().encode('UTF-8')   
     serverSocket.sendto(modifiedMessage, clientAdress)
